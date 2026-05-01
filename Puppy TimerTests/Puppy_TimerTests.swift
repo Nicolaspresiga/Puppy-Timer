4d52790bc8a52ec5ebe1f38081473ca93a816de3
Nicolas <presiga512@gmail.com>
2026-05-01 19:24:53 -0400

    Initial Commit

---
+++ Puppy TimerTests/Puppy_TimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Puppy_TimerTests.swift
+//  Puppy TimerTests
+//
+//  Created by Nicolas Marin Presiga on 5/1/26.
+//
+
+import Testing
+@testable import Puppy_Timer
+
+struct Puppy_TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Puppy TimerUITests/Puppy_TimerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Puppy_TimerUITestsLaunchTests.swift
+//  Puppy TimerUITests
+//
+//  Created by Nicolas Marin Presiga on 5/1/26.
+//
+
+import XCTest
+
+final class Puppy_TimerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
